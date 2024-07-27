2500bd12a0118f3527472a4932f55193a6a8fa5c
farbubby <farhan.mahbub@ucf.edu>
2024-07-27 18:57:33 -0400

    Added user sign-in functionality

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { signUpAction } from "@/server/sign-up";
+import { signInAction } from "@/server/sign-in";
 import { useFormState } from "react-dom";
 
 export default function Home() {
-  const [state, signUp] = useFormState(signUpAction, null);
+  const [state, signIn] = useFormState(signInAction, null);
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="text-white w-2/5 rounded-lg p-4 flex flex-col gap-4">
-          <div className="text-center font-bold">Sign up</div>
-          <form action={signUp} className="flex flex-col gap-6">
+          <div className="text-center font-bold">Sign in</div>
+          <form action={signIn} className="flex flex-col gap-6">
             <div className="flex flex-col text-sm gap-1">
+              {state?.fieldError?.error && (
+                <div className="text-red-500 text-sm">
+                  {state.fieldError.error}
+                </div>
+              )}
               <label htmlFor="email">Email</label>
               <input
                 type="email"
@@ -39,31 +44,17 @@ export default function Home() {
                 </div>
               )}
             </div>
-            <div className="flex flex-col text-sm gap-1">
-              <label htmlFor="username">Confirm password</label>
-              <input
-                type="text"
-                id="confirmPassword"
-                name="confirmPassword"
-                className="border border-white rounded-lg py-1 px-2 text-black"
-              />
-              {state?.fieldError?.confirmPassword && (
-                <div className="text-red-500 text-sm">
-                  {state.fieldError.confirmPassword}
-                </div>
-              )}
-            </div>
             <button
               type="submit"
               className="borde border-white py-1 px-2 rounded-lg bg-red-500 hover:bg-red-700 text-black duration-200">
-              Sign up
+              Sign in
             </button>
           </form>
           <div>
-            Already have an account?{" "}
+            Do not have an account?{" "}
             <span>
-              <a className="text-blue-500 hover:underline" href="/sign-in">
-                Sign in
+              <a className="text-blue-500 hover:underline" href="/">
+                Sign up
               </a>
             </span>
           </div>
---
+++ src/server/sign-in.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { z } from "zod";
+import { supabase } from "./supabase";
+import { redirect } from "next/navigation";
+
+const SignInSchema = z.object({
+  email: z.string().min(1, "Email is required").email(),
+  password: z.string().min(1, "Password is required"),
+});
+
+export async function signInAction(_: unknown, formData: FormData) {
+  const object = Object.fromEntries(formData.entries());
+  const parsed = SignInSchema.safeParse(object);
+
+  if (!parsed.success) {
+    const error = parsed.error.flatten();
+    return {
+      fieldError: {
+        email: error.fieldErrors.email?.[0],
+        password: error.fieldErrors.password?.[0],
+      },
+    };
+  }
+
+  const { email, password } = parsed.data;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return {
+      fieldError: {
+        error: error.message,
+      },
+    };
+  }
+
+  return null;
+
+  //return redirect("/sign-in");
+}
